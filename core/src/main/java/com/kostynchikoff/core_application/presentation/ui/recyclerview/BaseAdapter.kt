8e675ffef83c464062208b163532541e8c017746
Yergali <ergali.daniaruly@gmail.com>
2020-06-09 17:44:58 +0600

    1. Модифировал BaseAdapter

--- core/src/main/java/com/kostynchikoff/core_application/presentation/ui/recyclerview/BaseAdapter.kt
+++ core/src/main/java/com/kostynchikoff/core_application/presentation/ui/recyclerview/BaseAdapter.kt
@@ -1,15 +1,34 @@
 package com.kostynchikoff.core_application.presentation.ui.recyclerview
 
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
 import androidx.recyclerview.widget.RecyclerView
 
 abstract class BaseAdapter<T> : RecyclerView.Adapter<BaseViewHolder<T>>() {
+
     internal var items = mutableListOf<T>()
+
+    private var onItemClickListener: (any: Any) -> Unit = {}
 
     override fun onBindViewHolder(holder: BaseViewHolder<T>, position: Int) {
         holder.onBind(items[position])
     }
 
     override fun getItemCount(): Int = items.size
+
+    fun inflate(parent: ViewGroup, @LayoutRes layoutId: Int) : View {
+        return LayoutInflater.from(parent.context).inflate(layoutId, parent, false)
+    }
+
+    fun setOnRvClickListener(onItemClickListener: (any: Any) -> Unit) {
+        this.onItemClickListener = onItemClickListener
+    }
+
+    fun onItemClick(any: Any) {
+        onItemClickListener.invoke(any)
+    }
 
     fun setItems(list: List<T>) {
         items.clear()
