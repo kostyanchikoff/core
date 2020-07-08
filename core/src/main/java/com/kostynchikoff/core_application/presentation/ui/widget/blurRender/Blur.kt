1b8aab5276ce189f23c641c5e74db6fc9d0b65d1
kostya <kostya.sherba97@gmail.com>
2020-07-08 21:32:30 +0600

    1. Добавлен blur layout

---
+++ core/src/main/java/com/kostynchikoff/core_application/presentation/ui/widget/blurRender/Blur.kt
@@ -0,0 +1,15 @@
+package com.kostynchikoff.core_application.presentation.ui.widget.blurRender
+
+import android.content.Context
+import android.graphics.Bitmap
+
+interface Blur {
+    fun prepare(
+        context: Context?,
+        buffer: Bitmap?,
+        radius: Float
+    ): Boolean
+
+    fun release()
+    fun blur(input: Bitmap?, output: Bitmap?)
+}
